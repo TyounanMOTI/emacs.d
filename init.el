0bea5c451741f1388d19a2d42c3a4cca8a179360
Hirotoshi Yoshitaka <tyounan.moti@gmail.com>
2015-10-17 14:06:25 +0000

    first commit

---
+++ init.el
@@ -0,0 +1,3 @@
+(package-initialize)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
