cdd3d3fb8c03f4aceb217a11c03dc15696600e0c
Hirotoshi Yoshitaka <tyounan.moti@gmail.com>
2015-10-17 14:18:06 +0000

    init init-loader

--- init.el
+++ init.el
@@ -1,3 +1,6 @@
 (package-initialize)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+(require 'init-loader)
+(init-loader-load)
