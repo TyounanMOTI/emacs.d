85fd8f85a3a68a9f8c4c3d20ff30720d09e930a6
Hirotoshi Yoshitaka <tyounan.moti@gmail.com>
2015-10-20 16:33:30 +0000

    fix package archive list did't load custom URL.

--- init.el
+++ init.el
@@ -1,6 +1,7 @@
-(package-initialize)
+(require 'package)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
+(package-initialize)
 (require 'init-loader)
 (init-loader-load)
