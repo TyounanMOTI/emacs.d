14ae3eb7109a474313d78e44bfa529feb22d214e
Hirotoshi Yoshitaka <tyounan.moti@gmail.com>
2015-10-22 13:34:16 +0000

    update package list

--- mypackage.el
+++ mypackage.el
@@ -2,12 +2,23 @@
 
 (defvar my/packages
   '(
-    auto-complete
-    helm
+    ac-c-headers
+    ac-helm
     ag
-    magit
     ample-theme
+    auto-complete
+    auto-package-update
+    flycheck
+    helm
+    helm-flycheck
+    init-loader
+    magit
     markdown-mode
+    paredit
+    r-spec
+    ruby-block
+    ruby-compilation
+    ruby-electric
     wgrep
     yasnippet
     ))
