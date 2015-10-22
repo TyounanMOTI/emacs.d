af8a000d51d9bcab26a029fcfa0dd915c1878902
Hirotoshi Yoshitaka <tyounan.moti@gmail.com>
2015-10-22 13:36:55 +0000

    set key "C-c g" to magit

--- inits/91_globalkeymap.el
+++ inits/91_globalkeymap.el
@@ -7,4 +7,5 @@
 (global-set-key (kbd "C-c i") 'package-install)
 (global-set-key (kbd "C-c e") 'eval-buffer)
 (global-set-key (kbd "C-c b") 'recompile)
+(global-set-key (kbd "C-c g") 'magit-status)
 
