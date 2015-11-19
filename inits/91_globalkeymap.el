e1495b0d27b1b6bbafa70c1b04f1751e4fb82498
Hirotoshi Yoshitaka <tyounan.moti@gmail.com>
2015-11-20 06:43:39 +0900

    add compile to global keymap

--- inits/91_globalkeymap.el
+++ inits/91_globalkeymap.el
@@ -7,6 +7,7 @@
 (global-set-key (kbd "C-c i") 'package-install)
 (global-set-key (kbd "C-c e") 'eval-buffer)
 (global-set-key (kbd "C-c b") 'recompile)
+(global-set-key (kbd "C-c c") 'compile)
 (global-set-key (kbd "C-c g") 'magit-status)
 
 ;; 画面分割
