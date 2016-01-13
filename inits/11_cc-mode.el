b608f00943389542f611c7e072c7347de2bb101d
Hirotoshi Yoshitaka <tyounan.moti@gmail.com>
2016-01-13 20:12:40 +0900

    fix C code style

--- inits/11_cc-mode.el
+++ inits/11_cc-mode.el
@@ -4,9 +4,8 @@
 ;; コーディングスタイルを変更
 (add-hook 'c++-mode-hook
 	  '(lambda()
-	     (c-set-style "stroustrup")
+	     (c-set-style "bsd")
 	     (setq indent-tabs-mode nil) ; インデントは空白
 	     (setq c-basic-offset 2)	 ; インデントは2文字
 	     (c-set-offset 'innamespace 0) ; namespaceの中はインデントしない
-	     (c-set-offset 'arglist-close 0) ; 関数の引数リストの閉じ括弧はインデントしない
 	     ))
